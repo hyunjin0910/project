405b1f3085cbe71a2251548d8556895a4267e928
Janet <xuanchen0910@gmail.com>
2022-02-13 00:48:57 +0900

    qna 답 연결하기

--- MBTI Test/js/start.js
+++ MBTI Test/js/start.js
@@ -2,18 +2,28 @@ const $main = document.querySelector("#main");
 const $qna = document.querySelector("#qna");
 
 
-function addAnswer(answerText){
+function addAnswer(answerText, qId){
     const $a = document.querySelector('.aBox');
     const answer = document.createElement('button');
+    answer.classList.add('answerList');
     $a.appendChild(answer); //소속 관계를 만들어 준다
+
     answer.innerHTML = answerText;
+    answer.addEventListener('click', function(){
+        const children = document.querySelectorAll('.answerList');
+        for(let i=0;i<children.length;i++){
+            children[i].disabled = true; //기존 보기 비활성화시키기
+            children[i].style.display ='none';
+        }
+        goNext(++qId);
+    }, false)
 }
 
 function goNext(qId){
     const $q = document.querySelector('.qBox');
     $q.innerHTML = qnaList[qId].q;
     for(let i in qnaList[qId].a){
-        addAnswer(qnaList[qId].a[i].answer);
+        addAnswer(qnaList[qId].a[i].answer, qId); //qId를 증가시키기 위해서 매개 변수에 추가함
     }
 }
 
