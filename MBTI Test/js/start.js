d6af7c95374e8665ed48b7ecfae3f3bc8d3d3ea5
Janet <xuanchen0910@gmail.com>
2022-02-12 22:45:26 +0900

    질문1 연결하기

--- MBTI Test/js/start.js
+++ MBTI Test/js/start.js
@@ -1,11 +1,23 @@
 const $main = document.querySelector("#main");
 const $qna = document.querySelector("#qna");
 
-function goNext(){
-    let $q = document.querySelector('.qBox');
-    $q.innerHTML = qnaList[0].q;
+
+function addAnswer(answerText){
+    const $a = document.querySelector('.aBox');
+    const answer = document.createElement('button');
+    $a.appendChild(answer); //소속 관계를 만들어 준다
+    answer.innerHTML = answerText;
 }
 
+function goNext(qId){
+    const $q = document.querySelector('.qBox');
+    $q.innerHTML = qnaList[qId].q;
+    for(let i in qnaList[qId].a){
+        addAnswer(qnaList[qId].a[i].answer);
+    }
+}
+
+//메인에서 qna로 넘어갈 때 
 function begin(){
     $main.style.webkitAnimation = "fadeOUt 1s";
     $main.style.animation = "fadeOUt 1s";
@@ -16,6 +28,7 @@ function begin(){
             $main.style.display = "none"
             $qna.style.display = "block"
         },450)
-        goNext();
+        let qId=0;
+        goNext(qId);
     },450);
 }
