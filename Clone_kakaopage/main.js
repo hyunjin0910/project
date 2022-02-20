ebe6e0eef06400ddc487886f9d29915adddb456c
Janet <xuanchen0910@gmail.com>
2022-02-20 18:13:24 +0900

    [fix] change foler name : remove space

--- Clone_kakaopage/main.js
+++ Clone_kakaopage/main.js
@@ -3,20 +3,20 @@ const $navbar__menu = document.querySelectorAll(".navbar__menu>button");
 const $nav__home = document.querySelector("#nav__home");
 const $nav__toon = document.querySelector("#nav__toon");
 
-//웹툰 클릭 시
-const toToon = function(){
-    document.querySelector(".contents__genre").style.display = "block"
-    document.querySelector(".category").style.display = "none";
-    document.querySelector(".waiting").style.display = "none";
-}
+// //웹툰 클릭 시
+// const toToon = function(){
+//     document.querySelector(".contents__genre").style.display = "block"
+//     document.querySelector(".category").style.display = "none";
+//     document.querySelector(".waiting").style.display = "none";
+// }
 
-//네비바 다른 탭 클릭 시
-const toHome = function(){
-    document.querySelector(".contents__genre").style.display = "none"
-    document.querySelector(".category").style.display = "block";
-    document.querySelector(".waiting").style.display = "block";
-    $banner_image.src="./forCSS/kakao.jpeg";
-}
+// //네비바 다른 탭 클릭 시
+// const toHome = function(){
+//     document.querySelector(".contents__genre").style.display = "none"
+//     document.querySelector(".category").style.display = "block";
+//     document.querySelector(".waiting").style.display = "block";
+//     $banner_image.src="./forCSS/kakao.jpeg";
+// }
 
 for(let x of $navbar__menu){
     if(x===$nav__toon){
