2cb9dc3ebed449787760ecb4d239de2ba591bac3
Janet <xuanchen0910@gmail.com>
2022-02-21 01:53:29 +0900

    [design] daily contents: underline&text color - when clicked

--- Clone_kakaopage/main.js
+++ Clone_kakaopage/main.js
@@ -1,6 +1,7 @@
 const CSSstyle = getComputedStyle(document.body);
 const textcolor = CSSstyle.getPropertyValue('--text-color');
-const darkgrey = CSSstyle.getPropertyValue('--dark-grey')
+const darkgrey = CSSstyle.getPropertyValue('--dark-grey');
+const accent = CSSstyle.getPropertyValue('--accent-color');
 
 // 1. 장르 선택 시 글씨 색 변경
 //1) 글씨 색 변경
@@ -23,6 +24,27 @@ function genreClickHandler(event) {
     }
     curGenre= target;
   }
+$$genrebar.addEventListener('click',genreClickHandler);
 
+//2. 요일 연재
+//1) 요일 글씨 색, 밑줄 변화
+const $$days= document.querySelector(".days");
+const $days= document.querySelector(".days__Mon");
+const $c_img= document.querySelector(".c_img");
+let curdays= $days;
 
+function daysClickHandler(event) {
+    let target= event.target;
+    if(target.tagName!="BUTTON"){
+        return;
+    } //버튼 아래 밑줄 방지
+    if(target!=curdays){
+        target.style.color= textcolor;
+        target.style.borderBottom= "4px solid #FCBC19";
+        curdays.style.color= darkgrey;
+        curdays.style.borderColor="#FFFFFF";
+    }
+    curdays= target;
+  }
 
+$$days.addEventListener('click',daysClickHandler);
