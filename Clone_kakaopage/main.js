89f314c5f52ec53bb8068c8d9a519eb43e3f64b4
Janet <xuanchen0910@gmail.com>
2022-02-20 23:46:59 +0900

    [feat] genrebar: display none when !=home

--- Clone_kakaopage/main.js
+++ Clone_kakaopage/main.js
@@ -1,46 +1,9 @@
-var CSSstyle = getComputedStyle(document.body);
-// CSSstyle.getPropertyValue('--text-color');
+const CSSstyle = getComputedStyle(document.body);
+const textcolor = CSSstyle.getPropertyValue('--text-color');
+const darkgrey = CSSstyle.getPropertyValue('--dark-grey')
 
-//1. 네비바 클릭 시
-const $navbar__menu = document.querySelectorAll(".navbar__menu>button");
-const $nav__home = document.querySelector("#nav__home");
-const $nav__toon = document.querySelector("#nav__toon");
-
-//웹툰 클릭 시
-// const toToon = function(){
-//     document.querySelector(".contents__genre").style.display = "block"
-//     document.querySelector(".category").style.display = "none";
-//     document.querySelector(".waiting").style.display = "none";
-// }
-
-//네비바 다른 탭 클릭 시
-// const toHome = function(){
-//     document.querySelector(".contents__genre").style.display = "none"
-//     document.querySelector(".category").style.display = "block";
-//     document.querySelector(".waiting").style.display = "block";
-//     $banner_image.src="./forCSS/kakao.jpeg";
-// }
-
-// for(let x of $navbar__menu){
-//     if(x===$nav__toon){
-//         x.addEventListener('click', toToon);
-//     }else{
-//         x.addEventListener('click', toHome);
-//     }
-// }
-
-//2. 장르 선택 시 배너 사진 변경
-
-
-// for(let x in $genrebar){
-//     let newSrc = "./forCss/Genre/"+(Number(x)+1)+".png"
-//     function changeBanner(){
-//         $banner_image.src = newSrc;
-//     }
-//     $genrebar[x].addEventListener('click',changeBanner)
-// }
-
-// 장르 선택 시 글씨 색 변경
+// 1. 장르 선택 시 글씨 색 변경
+//1) 글씨 색 변경
 const $$genrebar= document.querySelector(".genrebar");
 const $genrebar= document.querySelector(".genrebar__tab");
 const $banner_image= document.querySelector(".banner__image")
@@ -48,12 +11,18 @@ let curGenre = $genrebar;
 function genreClickHandler(event) {
     let target= event.target;
     if(target!=curGenre){
-        target.style.color = CSSstyle.getPropertyValue('--text-color');
-        curGenre.style.color = CSSstyle.getPropertyValue('--dark-grey');
+        target.style.color = textcolor;
+        curGenre.style.color = darkgrey;
+    }
+    if(target!=$genrebar){
+        document.querySelector(".daily").style.display ='none';
+        document.querySelector(".banner").style.display ='none';
+    }else{
+        document.querySelector(".daily").style.display ='block';
+        document.querySelector(".banner").style.display ='block';
     }
     curGenre= target;
   }
 
-$$genrebar.addEventListener('click',genreClickHandler)
 
 
