cf68526585fcf38af12f436643a4e8429cad4519
Janet <xuanchen0910@gmail.com>
2022-03-13 17:48:22 +0900

    [feat] upadate ordered menu list and number

--- MoonBurks/main.js
+++ MoonBurks/main.js
@@ -1,10 +1,47 @@
+const $=(selector)=>document.querySelector(selector);
+const $$=(selector)=>document.querySelectorAll(selector);
+
 function typeMenu(){
-    document
-        .querySelector("#menu-name")
+    //form 자동 전송 방지
+    $("#order-form")
+        .addEventListener('submit',(e)=>{
+            e.preventDefault();
+        })
+
+    $("#menu-name")
         .addEventListener('keypress',(e)=>{
             if(e.key==='Enter'){
-                console.log(document.querySelector("#menu-name").value)
+              putMenu();
             }
         })
 }
+function putMenu(){
+    const orderedMenu= $("#menu-name").value;
+    const menuTemplate= (orderedMenu)=>{
+    return `
+        <li class="menu-list-item d-flex items-center py-2">
+    <span class="w-100 pl-2 menu-name">${orderedMenu}</span>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+    >
+      수정
+    </button>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+    >
+      삭제
+    </button>
+  </li>`
+    }
+    $("#espresso-menu-list").insertAdjacentHTML('beforeend',menuTemplate(orderedMenu));
+    
+    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    $(".menu-count").innerText=`총 ${menuCount}개`;
+}
+
+//함수 실행
 typeMenu()
+
+
